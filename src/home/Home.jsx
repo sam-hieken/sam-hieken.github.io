151fba141402b9803fbc93aa46d67812a1e88f1e
Sam Hieken <sam.hieken@gmail.com>
2025-03-16 18:50:37 -0400

    Added TV interviews to homepage

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,6 +7,17 @@ const studentStatus = alumni
     ? "a recent graduate of Miami University with a Bachelor's degree in Computer Science and a minor in Economics" 
     : "currently a Senior year at Miami University majoring in Computer Science and minoring in Economics";
 
+const interviews = [
+    {
+        name: "Spectrum News (Cincinnati)",
+        video: "https://www.youtube.com/embed/-MI8SF8cKug?si=aDb-ex7DMXsmoT4G"
+    },
+    {
+        name: "WKRC Local 12 (Cincinnati)",
+        video: "https://mms.tveyes.com/MediaCenterPlayer.aspx?u=aHR0cDovL21lZGlhY2VudGVyLnR2ZXllcy5jb20vZG93bmxvYWRnYXRld2F5LmFzcHg%2FVXNlcklEPTMyNzMwNCZNRElEPTIyMzU5OTIxJk1EU2VlZD02ODk3JlR5cGU9TWVkaWE%3D"
+    }
+];
+
 /**
  * Home page
  */
@@ -15,6 +26,7 @@ export default function Home() {
         <h1>Sam Hieken</h1>
         <Picture/>
         <Bio/>
+        <Interviews/>
     </main>
 }
 
@@ -28,7 +40,7 @@ function Picture() {
 
 function Bio() {
     return <section className={style.bio}>
-        <p>
+        <p className={style.greeting}>
             Hello, 
         </p>
         <p>
@@ -40,5 +52,23 @@ function Bio() {
             I'm also the creator of <a href="https://congress.csi.miamioh.edu" target="_blank">Who Did I Elect?</a>, a site to provide voters reliable, non-partisan 
             information regarding their U.S. Congressmen.
         </p>
+    </section>
+}
+
+function Interviews() {
+    return <section className={style.interviews}>
+        <h2>See My TV Interviews</h2>
+        <ul className={style.interviewList}>
+            {
+                interviews.map((interview, i) => 
+                    <li className={style.interviewItem} key={i}>
+                        <section className={style.interview}>
+                            <h3>{interview.name}</h3>
+                            <iframe width="480" height="270" src={interview.video} title="Video Player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </section>
+                    </li>
+                )
+            }
+        </ul>
     </section>
 }
