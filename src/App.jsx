5b4d4613e878b6788c10402b9278354fe14cfbff
Sam Hieken <sam.hieken@gmail.com>
2025-04-11 00:21:37 -0400

    Created lightweight alternative to react helmet for changing page title and description meta by route

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,30 @@ import Resume from "./resume/Resume";
 import Experience from "./experience/Experience";
 import Projects from "./projects/Projects";
 
+import PageHead from "./util/component/PageHead";
+
 const routes = [
     {
         name: "Home",
+        description: "Welcome to my portfolio!",
         href: "/",
         component: Home
     },
     {
         name: "Resume",
+        description: "My resume",
         href: "/resume",
         component: Resume
     },
     {
         name: "Work Experience",
+        description: "My work experience",
         href: "/experience",
         component: Experience
     },
     {
         name: "Projects",
+        description: "My projects",
         href: "/projects",
         component: Projects
     }
@@ -34,16 +40,21 @@ const routes = [
  */
 export default function App() {
     return <HashRouter>
-        <Header/>
-        <main>
-            <Routes>
-                {
-                    routes.map((route, i) => 
-                        <Route key={i} path={route.href} element={<route.component/>}/>
-                    )
-                } 
-            </Routes>
-        </main>
-        <Footer/>
+        {/* <HelmetProvider> */}
+            <Header/>
+            <main>
+                <Routes>
+                    {
+                        routes.map((route, i) => 
+                            <Route key={i} path={route.href} element={<>
+                                <PageHead title={route.name} description={route.description}/>
+                                <route.component/>
+                            </>}/>
+                        )
+                    } 
+                </Routes>
+            </main>
+            <Footer/>
+        {/* </HelmetProvider> */}
     </HashRouter>
 }
---
+++ src/util/component/PageHead.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// Modified version of https://dev.to/rohitnirban/adding-page-titles-to-react-app-23oe
+// that allows a meta description.
+const PageHead = ({ title, description }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        document.title = title;
+
+        // Add or update meta description
+        const metaDescription = document.querySelector("meta[name='description']");
+        if (metaDescription) {
+            metaDescription.setAttribute("content", description);
+        } else {
+            const newMetaDescription = document.createElement("meta");
+            newMetaDescription.name = "description";
+            newMetaDescription.content = description;
+            document.head.appendChild(newMetaDescription);
+        }
+    }, [ location, title ]);
+
+    return null;
+};
+
+export default PageHead;
